e16ca2d86c59780516f4040fa4a53d051f17fc08
lunarwaterfox <lunarwaterfox@gmail.com>
2019-06-16 21:48:57 +0800

    add jekyll setting

--- lisp/init-jekyll-project.el
+++ lisp/init-jekyll-project.el
@@ -1,3 +1,8 @@
 (require 'jekyll-project)
 
+(global-set-key (kbd "C-c o") 'jekyll-project-org-open)
+(global-set-key (kbd "C-c e") 'jekyll-project-org-export)
+(global-set-key (kbd "C-c r") 'jekyll-project-run-serve)
+(global-set-key (kbd "C-c s") 'jekyll-project-show-web)
+
 (provide 'init-jekyll-project)
