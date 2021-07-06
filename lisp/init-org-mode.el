17c2421e596e4ce2046afae11f37ca2efd9466cf
lunarwaterfox <lunarwaterfox@gmail.com>
2021-07-06 22:30:27 +0800

    setup org mode

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -6,4 +6,24 @@
 ;(with-eval-after-load 'org
 ;  (add-to-list 'org-export-backends 'md))
 
+
+;; todo list
+(setq org-todo-keywords
+      '((sequence "TODO" "|" "DONE" "CANCELED")))
+
+(setq org-todo-keyword-faces
+      '(("TODO" . "yellow")
+        ("DONE" . (:foreground "green" :weight bold))
+        ("CANCELED" . (:foreground "red" :weight bold))))
+
+;; mobile setting
+(setq org-directory "~/Documents/org")
+(setq org-mobile-inbox-for-pull "~/Documents/org/inbox.org")
+(setq org-mobile-files
+      '("~/Documents/org/task.org"))
+(setq org-mobile-directory "~/Dropbox/应用/MobileOrg")
+
+(setq org-mobile-use-encryption t)
+(setq org-mobile-encryption-password "DropboxOrg")
+
 (provide 'init-org-mode)
